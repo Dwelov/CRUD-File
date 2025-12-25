7a20c5f5ec92b7c86dd3e6383e8b5d65db4cf5f2
Dwelov <sirajmeher984@gmail.com>
2025-12-25 12:44:19 +0500

    more changes

---
+++ validation.cpp
@@ -0,0 +1,39 @@
+#include "validation.h"
+
+bool validateRollNumber(const string &rollNumber)
+{
+    // as my roll no is the dataset file are in the format BSAI25011519-50
+    // Step # 1
+    if (rollNumber.length() != 15)
+    {
+        return false;
+    }
+
+    // step # 2-checking first 4 characters
+    if (rollNumber.substr(0, 4) != "BSAI")
+    {
+        return false;
+    }
+
+    // step # 3-checking next 8 characters are digits or not
+    for (int i = 4; i < 12; i++)
+    {
+        if (!isdigit(rollNumber[i]))
+        {
+            return false;
+        }
+    }
+
+    // step # 4 - checking if the 8 characters are equal to 25011519
+    for (int i = 4; i < 12; i++)
+    {
+        string result;
+        result += rollNumber[i];
+        if (stoi(result) != 25011519)
+        {
+            return false;
+        }
+    }
+
+    
+}
---
+++ validation.h
@@ -0,0 +1,12 @@
+#ifndef VALIDATION.H
+#define VALIDATION .H
+
+#include <string>
+#include <iostream>
+#include <string>
+#include <cctype>
+using namespace std;
+
+bool validateRollNumber(const string &rollNumber);
+
+#endif
