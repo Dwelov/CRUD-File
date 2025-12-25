d5e5744b0e38874997e598dbd04ab321225d6480
Dwelov <sirajmeher984@gmail.com>
2025-12-25 12:52:39 +0500

    successfully develop the validation.cpp

--- main.cpp
+++ main.cpp
@@ -1,14 +1,18 @@
 
 #include "search.h"
+#include "validation.h"
 using namespace std;
 
-int main() {
-
+int main()
+{
     string index;
     cout << "Enter index to search: ";
     cin >> index;
-
+    if (!validateRollNumber(index))
+    {
+        cout << "Invalid roll number format." << endl;
+        return 1;
+    }
     search(index);
-
     return 0;
 }
