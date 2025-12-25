d5ceaf605438ea183e818becd3f565a8d4e86e7d
Dwelov <sirajmeher984@gmail.com>
2025-12-25 16:33:54 +0500

    successfully created the add students record function

--- main.cpp
+++ main.cpp
@@ -1,10 +1,49 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <limits>
 #include "crud.h"
 #include "search.h"
 #include "validation.h"
+
 using namespace std;
 
 int main()
 {
-    string filename;
-    
+    string filename, rollNo, name;
+    vector<int> marksList;
+    float average;
+
+    cout << "Enter the filename you want to update: ";
+    cin >> filename;
+
+    cout << "Now Enter the roll number: ";
+    cin >> rollNo;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    cout << "Now Enter the name of student: ";
+    getline(cin, name);
+    cout<<endl;
+
+    if (!validateRollNumber(rollNo))
+    {
+        cerr << "Roll no should be in the format BSAI25011519-(two digit number)" << endl;
+        return 1;
+    }
+
+    for (int i = 0; i < 7; i++)
+    {
+        int mark;
+        cout << "Enter mark " << i + 1 << ": ";
+        cin >> mark;
+        marksList.push_back(mark);
+    }
+
+    cout << "Now enter the average of the marks: ";
+    cin >> average;
+
+    addStudents(filename, rollNo, name, marksList, average);
+
+
+    return 0; 
 }
