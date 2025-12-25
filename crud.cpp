c6acd9497cb9a54d476c9440c7498258b594b8ba
Dwelov <sirajmeher984@gmail.com>
2025-12-25 15:37:04 +0500

    successfully created the function to add the new students

--- crud.cpp
+++ crud.cpp
@@ -17,3 +17,22 @@ void readFile(const string &filename)
     }
     file.close();
 }
+
+void addStudents(const string &filename, const string &rollNumber, const string &newName,
+                 const vector<int> &newMarks, double newAverage)
+{
+    ofstream file(filename, ios::app); // append mode
+    if (!file)
+    {
+        cerr << "Error: file not found." << endl;
+        return;
+    }
+    // Write the new record in CSV format file << rollNumber << "," << newName;
+    for (int mark : newMarks)
+    {
+        file << "," << mark;
+    }
+    file << "," << newAverage << "\n";
+    file.close();
+    cout << "Student record added successfully!" << endl;
+}
--- crud.h
+++ crud.h
@@ -3,7 +3,11 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
 using namespace std;
 // Function to read the existing file
 void readFile(const string &filename);
+void addStudents(const string &filename, const string &rollNumber, const string &newName,
+                  const vector<int> &newMarks, double newAverage);
+
 #endif
