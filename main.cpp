b35dfc3f00071f170c46c8f73f6fa6d4a51d7ee5
Dwelov <sirajmeher984@gmail.com>
2025-12-25 10:45:47 +0500

    add files

---
+++ main.cpp
@@ -0,0 +1,14 @@
+
+#include "search.h"
+using namespace std;
+
+int main() {
+
+    string index;
+    cout << "Enter index to search: ";
+    cin >> index;
+
+    search(index);
+
+    return 0;
+}
