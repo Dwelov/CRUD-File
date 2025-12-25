7861703641165d80eb1afb0c0b4a54ced98c54e6
Dwelov <sirajmeher984@gmail.com>
2025-12-25 14:34:44 +0500

    fixing teh bugs

---
+++ crud.h
@@ -0,0 +1,9 @@
+#ifndef CRUD_CPP
+#define CRUD_CPP
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+// Function to read the existing file
+void readFile(const ifstream &filename);
+#endif
