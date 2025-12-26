b22d07f9f3638e101250fad73946ce9e6a99f528
Dwelov <sirajmeher984@gmail.com>
2025-12-26 21:57:01 +0500

    finally completed all the files

--- crud.h
+++ crud.h
@@ -8,24 +8,22 @@
 #include <sstream>
 using namespace std;
 
-     //function to create the file
-     void createFile( const string &filename);
+// function to create the file
+void createFile(const string &filename);
 
-    // Function to read the existing file
-     void readFile(const string &filename);
+// Function to read the existing file
+void readFile(const string &filename);
 
-               //function to add students in the file
-     void addStudents(const string &filename, const string &rollNumber, const string &newName,
+// function to add students in the file
+void addStudents(const string &filename, const string &rollNumber, const string &newName,
                  const vector<int> &newMarks, float newAverage);
 
-                 //function to update the file
-     void updateRecord(const string &filename, const string &rollNumber, const string &newName,
+// function to update the file
+void updateRecord(const string &filename, const string &rollNumber, const string &newName,
                   const vector<int> &newMarks, float newAverage);
 
-      //function to delete the file
+// function to delete the file
 
-      void deleteRecord(const string &filename, const string rollNumber);
-
-
+void deleteRecord(const string &filename, const string &rollNumber);
 
 #endif
